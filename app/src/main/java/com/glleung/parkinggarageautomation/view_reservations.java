55cb1ce35d5f243ef066eb004ac886461463e996
Alex <aorsini2017@gmail.com>
2016-04-27 01:01:00 -0400

    Added reservation adding, other small fixes

---
+++ app/src/main/java/com/glleung/parkinggarageautomation/view_reservations.java
@@ -0,0 +1,13 @@
+package com.glleung.parkinggarageautomation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class view_reservations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reservations);
+    }
+}
